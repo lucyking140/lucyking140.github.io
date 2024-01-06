23525894d282b7a3b22fec718580d189938b3c83
Lucy King <lucyking140@gmail.com>
2024-01-06 11:01:23 -0800

    added image carousel to project pages

--- src/pages/project.js
+++ src/pages/project.js
@@ -1,5 +1,7 @@
 import React from "react";
 import '/Users/lucyking/Projects/portfolio/lk-portfolio/src/App.css';
+import Caro from "/Users/lucyking/Projects/portfolio/lk-portfolio/src/components/Caro"
+
  
 const ProjectPage = ({title, desc, skillList, url, github, photo1, photo2, photo3}) => {
     return (
@@ -7,23 +9,17 @@ const ProjectPage = ({title, desc, skillList, url, github, photo1, photo2, photo
 
             <h1>{title}</h1>
 
-            <div style={{ display: 'flex', gap: '20px' }} >
+            <div style={{ display: 'flex', gap: '20px', padding: '15px 0px'}} >
                 <button> <a href={url}> View the site </a></button>
                 <button> <a href={github}> Github Repo </a></button>
             </div>
             
-
             <h3> Tools Used:  {skillList} </h3>
 
             {desc}
 
-            <h3> Example pages: </h3>
-            <img src={photo1} alt = "Auction Alchemy Example" />
-            <p></p> 
-            <img src={photo2} alt = "Auction Alchemy Example" />
-            <p></p> 
-            <img src={photo3} alt = "Auction Alchemy Example" />
-            
+            <Caro photo1={photo1} photo2={photo2} photo3={photo3} />
+
             <div className="content"> </div>
         </div>
     );
